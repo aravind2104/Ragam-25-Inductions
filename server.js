93ba0fde4ab221f514d058ce605b89485acafb7d
Aravind <aravindanil2101@gmail.com>
2024-12-07 19:14:57 +0530

    Server work

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api', bookRoutes);
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>  console.log('Connected to Database'))
+    .catch(err => console.log('Connection Error: ', err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
